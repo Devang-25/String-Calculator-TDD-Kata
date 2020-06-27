605682e022edd694a02e049984e0945480ffb8e0
Devang Sharma <devshu25@gmail.com>
2020-06-28 02:49:13 +0530

    1 & 2 - adding empty to unknown number of integers separated by commas for Main Class

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,5 +1,18 @@
-public class StringCalculator {
-    public int add(String numbers){
-        return 0;
+public class StringCalculator 
+{
+    public int add(String numbers)
+{
+
+if (numbers.equals(""))
+            return 0;
+
+        String[] sNumbers = numbers.split(",");
+        int count = 0;
+        for(String v : sNumbers)
+       {
+            count += Integer.parseInt(v.trim());
+        }
+        return count;
+
     }
 }
