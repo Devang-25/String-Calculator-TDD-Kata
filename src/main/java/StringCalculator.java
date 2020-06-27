761424eca5133c2779479644e5d32748a44a4255
Devang Sharma <devshu25@gmail.com>
2020-06-28 02:53:41 +0530

    4-Support for different delimiters in Main Class

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -6,7 +6,14 @@ public class StringCalculator
 if (numbers.equals(""))
             return 0;
 
-         String[] sNumbers = numbers.split("[,\n]");
+         String customDelimiter;
+       if (numbers.contains("//")) {
+            int newLineIndex = numbers.indexOf('\n');
+            customDelimiter = numbers.substring(2, newLineIndex);
+            numbers = numbers.substring(newLineIndex + 1);
+        } else
+            customDelimiter = "";
+        String[] sNumbers = numbers.split("[,\n" + customDelimiter + "]");
         int count = 0;
         for(String v : sNumbers)
        {
