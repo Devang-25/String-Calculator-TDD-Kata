d461901dbb8d39303c95e2f7ae0d6d533034769e
Devang Sharma <devshu25@gmail.com>
2020-06-28 02:51:19 +0530

    3-Handled new lines between numbers in Test Class

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -24,5 +24,10 @@ public class StringCalculatorTest {
         Assert.assertEquals(3, stringCalculator.add("1,2"));
     }
 
+@Test
+    public void addNumbersWithNewLine(){
+        Assert.assertEquals(6, stringCalculator.add("1\n2,3"));
+    }
+
 }	}
 }
