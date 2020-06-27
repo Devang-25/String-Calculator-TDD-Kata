7493f8acc15a3496c1b6dda81ff3f486ed8684c8
Devang Sharma <devshu25@gmail.com>
2020-06-28 03:12:39 +0530

    11-Allowing multiple delimiters in Test Class

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -72,5 +72,11 @@ public class StringCalculatorTest {
     }
 
 
+ @Test
+    public void allowingMultipleDelimiters(){
+        Assert.assertEquals(6, stringCalculator.add("//[*][%]\n1*2%3"));
+    }
+
+
 }	}
 }
